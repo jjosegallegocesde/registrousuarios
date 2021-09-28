c2349f91bbe33ad6c2e0e3d051f6910855c66c2f
JuanJo <jjosegallegomesa@gmail.com>
2021-09-27 19:29:20 -0500

    se crea formulario

--- index.php
+++ index.php
@@ -8,6 +8,56 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
 </head>
 <body>
+    <header>
+
+    </header>
+    <main>
+
+        <div class="container">
+            <div class="row d-flex justify-content-center">
+                <div class="col-12 col-md-5">
+
+                 <form action="" method="">
+
+                    <div class="mb-3">
+                        <label class="form-label">Nombres</label>
+                        <input type="text" class="form-control">
+                    </div>
+
+                    <div class="mb-3">
+                        <label class="form-label">Apellidos</label>
+                        <input type="text" class="form-control">
+                    </div>
+
+                    <div class="mb-3">
+                        <label class="form-label">Email</label>
+                        <input type="email" class="form-control">
+                    </div>
+
+                    <div class="mb-3">
+                        <label class="form-label">Edad</label>
+                        <input type="number" class="form-control">
+                    </div>
+
+                    <div class="mb-3">
+                        <label class="form-label">Descripcion</label>
+                        <textarea class="form-control" placeholder="Leave a comment here"></textarea>
+                    </div>
+
+                    <div class="mb-3">
+                        <label class="form-label">Fotografia</label>
+                        <input type="text" class="form-control">
+                    </div>
+            
+                </form>
+
+                </div>
+            </div>
+        </div>
+
+       
+
+    </main>
     
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
 </body>
